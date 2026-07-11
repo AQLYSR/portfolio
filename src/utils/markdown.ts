80e307966db04ccfb3015fca3b188116af2a0b1b
AQLYSR <aqilyusri@proton.me>
2026-07-11 12:57:25 +0800

    fix markdown body text turn to $1

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -14,7 +14,7 @@ export function markdownToHtml(md: string): string {
     .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>')
     .replace(/^- (.+)$/gm, '<li>$1</li>');
 
-  html = html.replace(/^(?!<[a-zA-Z/])[^\n]+$/gm, '<p>$1</p>');
+  html = html.replace(/^(?!<[a-zA-Z/])([^\n]+)$/gm, '<p>$1</p>');
   html = html.replace(/<p>\s*<\/p>/g, '');
 
   return html;
